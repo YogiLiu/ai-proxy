6af157becedd83186209bb41fba50486e7ca51b0
YogiLiu <YogiLiu@outlook.com>
2025-08-31 22:24:16 +0800

    fix(proxy): fix regex of path

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,11 +1,13 @@
 import { Hono } from 'hono'
+import { logger } from 'hono/logger'
 import { proxy } from 'hono/proxy'
 
 const app = new Hono()
+app.use(logger())
 export default app
 
 app.all(
-    '/:domain{(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)(?:\\.(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?))+}/:path',
+    '/:domain{(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)(?:\\.(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?))+}/:path{.+}',
     (c) => {
         const { domain, path } = c.req.param()
         return proxy(`https://${domain}/${path}`, {
